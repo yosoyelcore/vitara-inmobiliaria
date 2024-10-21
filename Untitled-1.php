69d83c701dc1fa742e0fa7d4c3abd78444b5a992
Miguel Coreño <119348048+yosoyelcore@users.noreply.github.com>
2024-10-21 11:16:11 -0600

    Prueba

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,10 +1,10 @@
 <?php
-function sum($a, $b) {
+function sum($a, $b): mixed {
     return $a + $b;
 }
 
 // Example usage
-$num1 = 5;
+/$num1 = 5;
 $num2 = 10;
 echo "The sum of $num1 and $num2 is: " . sum($num1, $num2);
 ?>
